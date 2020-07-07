8da90c52650e61f1ff7eb2d0982db196c814d55c
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-07 10:22:03 -0400

    Initial Setup of CardMaker

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+import CardMaker from './components/CardMaker';
+
 
 axios.get('https://api.github.com/users/MaryamMosstoufi')
   .then(response => {
     //get user api response
     console.log('user api response', response)
-    
+
     // get followers api url
     console.log('followers api url', response.data.followers_url);
 
@@ -28,6 +30,7 @@ function App() {
   return (
     <div className="App">
       <h1>React Github User Card</h1>
+      <CardMaker/>
     </div>
   );
 }
---
+++ src/components/CardMaker.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CardMaker extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      card: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>Each Card</div>
+     )
+  }
+}
+export default CardMaker;
