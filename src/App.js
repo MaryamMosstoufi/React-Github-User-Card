90a0d061e5932d46ef90a39786439bd10d0ec059
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-07 20:49:08 -0400

    User Card + Followers Card

--- src/App.js
+++ src/App.js
@@ -3,36 +3,60 @@ import axios from 'axios';
 import './App.css';
 
 import CardMaker from './components/CardMaker';
+import UserCard from './components/UserCard';
 
+class App extends React.Component {
+  state = {
+    userName:'MaryamMosstoufi',
+    user: [], 
+    followersUrl: '',
+    followers: []
+  };
 
-axios.get('https://api.github.com/users/MaryamMosstoufi')
-  .then(response => {
-    //get user api response
-    console.log('user api response', response)
-
-    // get followers api url
-    console.log('followers api url', response.data.followers_url);
-
-    axios.get(response.data.followers_url)
-      .then(response => {
-        // get followers api response
-        console.log('followers api response',response.data);
+  componentDidMount() {
+    console.log(this.mainUser)
+    axios
+      .get(`https://api.github.com/users/${this.state.userName}`)
+      .then(res => {
+        console.log('Main User API Call', res)
+        //console.log(res.data.followers_url)
+        this.setState({
+          user: res.data,
+          followersUrl: res.data.followers_url
+        }); 
+        axios
+          .get(this.state.followersUrl)
+          .then(res => {
+            console.log('Followers URL API Call', res)
+            this.setState({
+              followers: res.data
+            });
+          })
+          .catch(error => {
+            console.log("Get Followers Error:", error);
+          })
       })
       .catch(error => {
-        console.log("Error:", error);
+        console.log("Get Followers URL Error:", error);
       })
+    console.log('component did mount')
+    
+  }
 
-  })
-  .catch(error => {
-    console.log("Error:", error);
-  })
-function App() {
-  return (
-    <div className="App">
-      <h1>React Github User Card</h1>
-      <CardMaker/>
+  
+  render() {
+    return (
+    <div className="container">
+        <h1>React Github User Card</h1>
+        <UserCard user={this.state.user}/> 
+        <div>
+          {this.state.followers.map(follower => (
+            <CardMaker  key={follower.id} follower={follower} />
+          ))}
+        </div>
     </div>
   );
+  }
 }
 
 export default App;
--- src/components/CardMaker.js
+++ src/components/CardMaker.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-class CardMaker extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      card: ''
-    };
-  }
+class CardMaker extends React.Component  {
 
   render() {
     return (
-      <div>Each Card</div>
-     )
+      <div className="card">
+        <img src={this.props.follower.avatar_url} alt='Avatar'/>
+        <div className="card-info">
+          <p className="username">{this.props.follower.login}</p>
+          <a href={this.props.follower.html_url}>View Github Profile</a>
+        </div>
+      </div>
+    )
   }
 }
 export default CardMaker;
